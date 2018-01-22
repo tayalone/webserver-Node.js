fa133daa238217d7bde43abee5e91615f908da23
panupong <tayalone@gmail.com>
2018-01-22 14:12:52 +0700

    test some express middle ware

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const hbs = require('hbs')
+const fs = require('fs')
 
 const app = express()
 
@@ -7,6 +8,23 @@ const app = express()
 hbs.registerPartials(__dirname+'/views/partials')
 
 app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+    const now = new Date().toString()
+    const log= `${now}: ${req.method} ${req.url}`
+    
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    })
+    next()
+})
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs')
+// })
+
 // ใช้ middleware ทำให้ express แสดงstatic page ได้
 app.use(express.static(__dirname + '/public'))
 
