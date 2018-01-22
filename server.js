6e72fb45bc7afdefbcc65e1c03cabbc687935db9
panupong <tayalone@gmail.com>
2018-01-22 13:24:58 +0700

    use template engine "hbj" for create dynamic html page

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
 
+app.set('view engine', 'hbs')
 // ใช้ middleware ทำให้ express แสดงstatic page ได้
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
     // res.send('<h1>Hello Express!</h1>')
-    res.send({
-        name: 'Panupong',
-        pets: 'Cat' 
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: "Greeting Welcome to my page"
     })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About Page')
+    // res.send('About Page')
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    })
 })
 
 app.get('/bad', (req, res) => {
